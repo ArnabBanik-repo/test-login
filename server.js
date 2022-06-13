afc7f3acf44ceba6a64a3919c5826c3dbfc639ae
ArnabBanik-repo <arnabbanik9@gmail.com>
2022-06-13 15:44:25 +0530

    Fixed db

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 
-const app = require('./app');
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DB_URI);
+
+const app = require("./app");
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
